cda42ee8c86cc4d08182b410289deb967c16ee6e
Your Name <you@example.com>
2022-09-12 13:59:12 +0530

    dfgdg

--- controllers/student.js
+++ controllers/student.js
@@ -52,5 +52,6 @@ module.exports.uploadStudentDetails = async (req, res, next) => {
 
 module.exports.createPDF = async (req, res, next) => {
     console.log("create pdf API is called");
+    console.log("create pdf API is called");
     return req;
 }
